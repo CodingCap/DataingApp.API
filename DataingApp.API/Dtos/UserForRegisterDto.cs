e9d8b7f768a800c8d6f24f1ff0d9f6eb53a82b6d
Albu Valentin <albuvalentin23@yahoo.com>
2020-02-10 00:01:58 +0200

    Global Error handling

--- DataingApp.API/Dtos/UserForRegisterDto.cs
+++ DataingApp.API/Dtos/UserForRegisterDto.cs
@@ -4,11 +4,11 @@ namespace DataingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
-        [Required]
+        [Required(ErrorMessage = "Username cannot be empty")]
         public string UserName { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "specify password")]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "password must be at least 4 chars long")]
         public string Password { get; set; }
     }
 }
